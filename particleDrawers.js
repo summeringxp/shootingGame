f2ea973ed7c7e17eac2738c1022bcfdd9c531a34
peixia <summering_xp@hotmail.com>
2013-06-30 02:56:01 +0900

    add section

--- particleDrawers.js
+++ particleDrawers.js
@@ -1,8 +1,26 @@
-function drawArc(context, centerX,centerY,r,w,h){
+function drawArc(context,x,y,vx,vy,r,w,h,color){
+	context.beginPath();
+	var grd=ctx.createRadialGradient(x*w, y*h,r*Math.sqrt(w*w+h*h)/2,x*w,y*h,r*Math.sqrt(w*w+h*h));
+	grd.addColorStop(0,"white");
+	grd.addColorStop(1,color);
+	context.fillStyle=grd;
+    context.arc(x*w, y*h, r*Math.sqrt(w*w+h*h), 0, 2 * Math.PI, false);
+    context.fill();
+    
+}
+
+function testCharDrawer(context,x,y,vx,vy,w,h,color){
 	context.beginPath();
 	context.lineWidth = 2;
-    context.strokeStyle = '#'+Math.floor(Math.random()*16777215).toString(16);
-    context.arc(centerX*w, centerY*h, r*100, 0, 2 * Math.PI, false);
-    context.stroke();
+	context.fillStyle = color;
+    context.arc(x*w, y*h, 0.02*w, 0, 2 * Math.PI, false);
+    context.arc(x*w, y*h+0.002*h, 0.01*w, 0, 2 * Math.PI, false);
+    context.fill();
+   
+}
 
+function drawText(context,text,w,h){
+	context.fillStyle = "#000000";
+	context.font="10px Arial";
+	context.fillText(text,0.05*w,0.05*h);
 }
--- shootingEditor.js
+++ shootingEditor.js
@@ -8,51 +8,77 @@ function Particle(x,y,r,sx,sy,ax,ay){
 	this.ay = typeof ay !== 'undefined' ? ay : 0;
 	this.tx = typeof tx !== 'undefined' ? tx : 1;
 	this.ty = typeof ty !== 'undefined' ? ty : 1;
+	this.trackLevel = 0;
 }
-function ParticleGroup(){
-	this.number = 20;
+function ParticleGroup(number,drawer,color){
+	this.number = number;
 	this.particle = [];
 	this.bornTime = new Date().getTime();
 	this.pattern=[];
+	this.drawer = drawer;
+	this.color = color;
+	this.targetType = 0;
 }
 function Pattern(start,callback){
 	this.startTime=start;
 	this.callback = callback;
 }
-function defaultUpdateFun(particleGroup){
-	return particleGroup;
-}
-function defaultUpdateFun2(particleGroup){
+function defaultUpdateFun(particleGroup,x,y){
 	for(var j=0;j<particleGroup.particle.length;j++){
 		if(particleGroup.particle[j]==null){
 			continue;
 		}
-		particleGroup.particle[j].sx*=-2;
-		particleGroup.particle[j].sy*=-1;
+		particleGroup.particle[j].sx=0.0008*(x-particleGroup.particle[j].x)/Math.sqrt((x-particleGroup.particle[j].x)*(x-particleGroup.particle[j].x)+((y-particleGroup.particle[j].y)*(y-particleGroup.particle[j].y)));
+		particleGroup.particle[j].sy=0.0008*(y-particleGroup.particle[j].y)/Math.sqrt((x-particleGroup.particle[j].x)*(x-particleGroup.particle[j].x)+((y-particleGroup.particle[j].y)*(y-particleGroup.particle[j].y)));;
+		particleGroup.particle[j].trackLevel=0;
 	}
 	return particleGroup;
 }
-function defaultUpdateFun3(particleGroup){
+function defaultUpdateFun2(particleGroup,x,y){
 	for(var j=0;j<particleGroup.particle.length;j++){
 		if(particleGroup.particle[j]==null){
 			continue;
 		}
-		particleGroup.particle[j].sy*=-7;
+		var temp = particleGroup.particle[j].sx;
+		particleGroup.particle[j].sx=-particleGroup.particle[j].sy;
+		particleGroup.particle[j].sy=temp;
+		particleGroup.particle[j].trackLevel=0;
 	}
 	return particleGroup;
 }
-
-
-function createBasicGroup(){
-	var pg = new ParticleGroup();
+function defaultUpdateFun3(particleGroup,x,y){
+	for(var j=0;j<particleGroup.particle.length;j++){
+		if(particleGroup.particle[j]==null){
+			continue;
+		}
+		particleGroup.particle[j].tx=x;
+		particleGroup.particle[j].ty=y;
+		particleGroup.particle[j].trackLevel=1;
+	}
+	return particleGroup;
+}
+function createBasicGroup(number,drawer,color){
+	var pg = new ParticleGroup(number,drawer,color);
 	for(var i=0;i<pg.number;i++){
-		var p = new Particle(0.5,0.1,0.01,0.02*Math.cos(i),Math.abs(0.02*Math.sin(i)),0,0,0);
+		var p = new Particle(0.5,0.1,0.02,0.0001*Math.cos(i),Math.abs(0.0001*Math.sin(i)),0,0,0);
 		pg.particle.push(p);
 	}
-	var pattern1 = new Pattern(2000,defaultUpdateFun2);
+	var pattern1 = new Pattern(1500,defaultUpdateFun2);
 	pg.pattern.push(pattern1);
-	var pattern2 = new Pattern(3500,defaultUpdateFun3);
+	var pattern2 = new Pattern(3500,defaultUpdateFun);
 	pg.pattern.push(pattern2);
 	return pg;
-	
+}
+
+function createBasicGroup2(number,drawer,color){
+	var pg = new ParticleGroup(number,drawer,color);
+	for(var i=0;i<pg.number;i++){
+		var p = new Particle(0.5,0.1,0.02,0.0001*Math.cos(i),Math.abs(0.0001*Math.sin(i)),0,0,0);
+		pg.particle.push(p);
+	}
+	var pattern1 = new Pattern(0,defaultUpdateFun3);
+	pg.pattern.push(pattern1);
+	var pattern2 = new Pattern(1500,defaultUpdateFun);
+	pg.pattern.push(pattern2);
+	return pg;
 }
